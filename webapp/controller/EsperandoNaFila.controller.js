4f81b5785d2b2dc2ca2ec0c734a2a6b411989e84
Sebastian Goldberg <sebas.goldberg@gmail.com>
2017-11-13 11:56:03 -0200

    É adicionada a view EsperandoNaFila.

---
+++ webapp/controller/EsperandoNaFila.controller.js
@@ -0,0 +1,12 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function(Controller) {
+	"use strict";
+
+	return Controller.extend("iamsoft.filav.cliente.controller.EsperandoNaFila", {
+
+		onInit: function () {
+
+		}
+	});
+});
