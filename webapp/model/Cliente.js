ab9ffd5c29c10d5377a3edaac1d26fa743c49f85
Juan Sebastian Goldberg <sebas.goldberg@gmail.com>
2017-11-17 14:53:28 -0300

    A logica dos sockets foi passada para um repositorio independente e adicionado como um modulo.

--- webapp/model/Cliente.js
+++ webapp/model/Cliente.js
@@ -1,49 +1,15 @@
 sap.ui.define([
 	"sap/ui/base/Object",
-    "sap/ui/core/ws/WebSocket",
-], function (Object, WebSocket) {
+    "iamsoft/socket/model/Socket"
+], function (Object, Socket) {
 	"use strict";
-
-    let ChannelWebSocket = function(path="/fila/"){
-
-        this._socket = new channels.WebSocketBridge();
-        this._socket.connect(path);
-
-        this.listen = function(onMessage){
-            this._socket.listen(function (action, stream) {
-                onMessage(action);
-            });
-        };
-
-        this.send = function(data){
-            this._socket.send(data);
-        }
-
-    };
-
-
-    let UI5WebSocket = function(path="/fila/"){
-
-        this._socket = new WebSocket("ws://"+window.location.host+path);
-
-        this.listen = function(onMessage){
-            this._socket.attachMessage(this, function (oEvent) {
-                onMessage(JSON.parse(oEvent.getParameter('data')));
-            }, this);
-        };
-
-        this.send = function(data){
-            this._socket.send(JSON.stringify(data));
-        }
-
-    };
 
 	let instance;
 
 	let Cliente = Object.extend("iamsoft.filav.cliente.model.Cliente", {
 		
 		constructor: function () {
-			this._socket = new ChannelWebSocket();
+			this._socket = new Socket('/fila/');
 			this._socket.listen( data => {
                 if (data.message == 'QR_CODE'){
                     var eventBus = sap.ui.getCore().getEventBus();
